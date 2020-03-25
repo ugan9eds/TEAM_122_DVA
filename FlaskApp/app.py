7104cb8f09087699c4da749876743b8cf252d1d7
Nilay Bhatt <n.bhatt@optum.com>
2020-03-25 19:15:00 -0400

    base app

---
+++ FlaskApp/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+@app.route("/index")
+def main():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
+
---
+++ FlaskApp/models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Table
+from sqlalchemy import create_engine, MetaData, Table
+
+engine = create_engine("<CONNECTION_STRING_HERE>", convert_unicode=True)
+metadata = MetaData(bind=engine)
+
+
+player_stats = Table('player_stats', metadata, autoload=True)
