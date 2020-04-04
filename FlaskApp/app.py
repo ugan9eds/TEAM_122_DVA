5df247d6e10795d4883a178be7bfa7e6b89887b0
Nilay Bhatt <n.bhatt@optum.com>
2020-04-04 18:18:24 -0400

    created a basic d3 app with data being rendered from flask backend

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,12 +1,25 @@
+import json
+
 from flask import Flask, render_template
+import pandas as pd
+
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
-@app.route("/index")
-def main():
+def index():
     return render_template('index.html')
 
+
+@app.route("/getData", methods=['POST', 'GET'])
+def dataStream():
+    df = pd.read_csv('data/data.csv')
+    chart_data = df.to_dict(orient='records')
+    chart_data = json.dumps(chart_data, indent=2)
+    data = {'chart_data': chart_data}
+
+    return chart_data
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
